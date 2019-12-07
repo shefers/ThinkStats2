fbc1e17823cc3417cef026da979df2a3ee297341
shefers <51350968+shefers@users.noreply.github.com>
2019-12-06 21:36:21 -0600

    Add files via upload
    Shefers Sarkar DSC-530 Assignment 2.1

---
+++ Sarkar_Shefers_DSC530_Assignment_2.1.py
@@ -0,0 +1,32 @@
+#DSC530-T302
+# Assignment: 2.1
+#Shefers Sarkar
+#
+print ('Hello World! I wonder why that is always the default coding text to start with') #welcome message
+
+a=15
+b= 20
+c= 'Star'
+d= 'Wars'
+
+ADD = a+b
+Substract =b-a
+Multiply = a*b
+Divide = b/a
+
+print('The sum of two numbers:',ADD)
+print('The subtraction of two numbers:',Substract)
+print('The multiplication of two numbers:',Multiply)
+print('The Divition of two numbers:',Divide)
+print('Concatenate two strings together:', c +" "+ d)
+
+my_List = ["Jedi", "Order", "Galactic", "Empire"]
+print("My List:", my_List)
+my_List.append(6)
+print("My append List:", my_List)
+tuple1 = (0, 1, 2, 3)
+print("My Tupel:", tuple1)
+
+
+
+
