0bf8db909a17e363d60cf44533360104c12de39b
shefers <51350968+shefers@users.noreply.github.com>
2019-12-16 19:56:16 -0600

    Add files via upload

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -21,7 +21,8 @@ def Mode(hist):
 
     returns: value from Hist
     """
-    return 0
+    p, x = max([(p, x) for x, p in hist.Items()])
+    return x
 
 
 def AllModes(hist):
@@ -31,7 +32,39 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
+
+
+def WeightDifference(live, firsts, others):
+    """Explore the difference in weight between first babies and others.
+
+    live: DataFrame of all live births
+    firsts: DataFrame of first babies
+    others: DataFrame of others
+    """
+    mean0 = live.totalwgt_lb.mean()
+    mean1 = firsts.totalwgt_lb.mean()
+    mean2 = others.totalwgt_lb.mean()
+
+    var1 = firsts.totalwgt_lb.var()
+    var2 = others.totalwgt_lb.var()
+
+    print('Mean')
+    print('First babies', mean1)
+    print('Others', mean2)
+
+    print('Variance')
+    print('First babies', var1)
+    print('Others', var2)
+
+    print('Difference in lbs', mean1 - mean2)
+    print('Difference in oz', (mean1 - mean2) * 16)
+
+    print('Difference relative to mean (%age points)', 
+          (mean1 - mean2) / mean0 * 100)
+
+    d = thinkstats2.CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
+    print('Cohen d', d)
 
 
 def main(script):
@@ -42,14 +75,17 @@ def main(script):
     live, firsts, others = first.MakeFrames()
     hist = thinkstats2.Hist(live.prglngth)
 
+    # explore the weight difference between first babies and others
+    WeightDifference(live, firsts, others)
+
     # test Mode    
     mode = Mode(hist)
     print('Mode of preg length', mode)
-    assert mode == 39, mode
+    assert(mode == 39)
 
     # test AllModes
     modes = AllModes(hist)
-    assert modes[0][1] == 4693, modes[0][1]
+    assert(modes[0][1] == 4693)
 
     for value, freq in modes[:5]:
         print(value, freq)
